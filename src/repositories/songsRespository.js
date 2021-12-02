a88dd3f541843c128bcdce129b0161cd1e340bee
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-12-02 12:58:52 -0300

    created function to find song by id

--- src/repositories/songsRespository.js
+++ src/repositories/songsRespository.js
@@ -6,3 +6,11 @@ async function createSong(name, link) {
     [name, link]
   );
 }
+
+async function findSong(id) {
+  const result = await connection.query(`SELECT * FROM songs WHERE id = $1`, [
+    id,
+  ]);
+
+  return result.rows[0];
+}
