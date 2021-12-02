3d193afad44189979613c4ecd63f7b4bec521869
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-12-02 13:09:21 -0300

    created function to list top songs

--- src/repositories/songsRespository.js
+++ src/repositories/songsRespository.js
@@ -2,13 +2,13 @@ import connection from "../database.js";
 
 async function createSong(name, link) {
   await connection.query(
-    `INSERT INTO songs (name, "youtubeLink", score) VALUES ($1, $2, 1)`,
+    `INSERT INTO songs (name, "youtubeLink", score) VALUES ($1, $2, 1);`,
     [name, link]
   );
 }
 
 async function findSong(id) {
-  const result = await connection.query(`SELECT * FROM songs WHERE id = $1`, [
+  const result = await connection.query(`SELECT * FROM songs WHERE id = $1;`, [
     id,
   ]);
 
@@ -16,12 +16,24 @@ async function findSong(id) {
 }
 
 async function updateSongScore(id, score) {
-  await connection.query(`UPDATE songs SET score = $1 WHERE id = $2`, [
+  await connection.query(`UPDATE songs SET score = $1 WHERE id = $2;`, [
     score,
     id,
   ]);
 }
 
 async function deleteSong(id) {
-  await connection.query(`DELETE FROM songs WHERE id = $1`, [id]);
+  await connection.query(`DELETE FROM songs WHERE id = $1;`, [id]);
+}
+
+async function listTopSongs(limit) {
+  let query = `SELECT * FROM songs ORDER BY "score" DESC`;
+  if (limit) {
+    query += ` LIMIT $1;`;
+    const result = await connection.query(query, [limit]);
+  } else {
+    const result = await connection.query(query);
+  }
+
+  return result.rows;
 }
