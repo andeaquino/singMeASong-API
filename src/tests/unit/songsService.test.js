888fe6170de4a3dd9ec22b4945d206e9f339c919
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-12-04 18:57:42 -0300

    created unit tests for service function that changes a song score

--- src/tests/unit/songsService.test.js
+++ src/tests/unit/songsService.test.js
@@ -58,3 +58,44 @@ describe("Songs Service requesting random song", () => {
     expect(result.name).toEqual("normalSong");
   });
 });
+
+describe("Songs Service voting on a song", () => {
+  it("change score of a song to +1 when type = upvote", async () => {
+    jest
+      .spyOn(songsRepository, "findSong")
+      .mockImplementationOnce(() => ({ score: 1 }));
+    jest
+      .spyOn(songsRepository, "updateSongScore")
+      .mockImplementationOnce(() => ({ score: 2 }));
+
+    await sut.voteSong(1, "upvote");
+
+    expect(songsRepository.updateSongScore).toHaveBeenCalledWith(1, 2);
+  });
+
+  it("change score of a song to -1 when type = downvote", async () => {
+    jest
+      .spyOn(songsRepository, "findSong")
+      .mockImplementationOnce(() => ({ score: 1 }));
+    jest
+      .spyOn(songsRepository, "updateSongScore")
+      .mockImplementationOnce(() => ({ score: 0 }));
+
+    await sut.voteSong(1, "downvote");
+
+    expect(songsRepository.updateSongScore).toHaveBeenCalledWith(1, 0);
+  });
+
+  it("delete song if type = downvote and score = -5", async () => {
+    jest
+      .spyOn(songsRepository, "findSong")
+      .mockImplementationOnce(() => ({ score: -5 }));
+    jest
+      .spyOn(songsRepository, "deleteSong")
+      .mockImplementationOnce(() => null);
+
+    await sut.voteSong(1, "downvote");
+
+    expect(songsRepository.deleteSong).toHaveBeenCalledWith(1);
+  });
+});
